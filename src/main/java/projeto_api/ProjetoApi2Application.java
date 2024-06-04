53c09f14a7cf8f4668d0ab56a6be79fce48ab230
Jhonanthan Campos <dionathancampos@hotmail.com>
2024-06-04 09:19:28 -0300

    commit inicial

---
+++ src/main/java/projeto_api/ProjetoApi2Application.java
@@ -0,0 +1,13 @@
+package projeto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/projeto_api/ProjetoApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
